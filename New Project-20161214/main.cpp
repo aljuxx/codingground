0bbfb9b069b1fb1c57cd6fec235edc79389c4d28
aljuxx <riekstinacool@gmail.com>
2016-12-14 13:56:12 +0200

    Created by Coding Ground Client

---
+++ New Project-20161214/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   cout << "Hello World" << endl; 
+   cout << "Here, in the forest," << endl; 
+    cout << "dark and deep," << endl; 
+   cout << "I offer you," << endl; 
+   cout << "eternal sleep" << endl; 
+   int x;
+   int y; 
+   
+   x = 13;
+   y = 2016;
+   cout << "__________________________________________________________________________" << endl;
+   cout << "es sodien sanemu " << x << " davanas                                               |"<< endl;
+   cout << "es sogad uzdavinaju " << y << " davanas                                          |" << endl;
+   cout << "*Apveicu tevi " << y << " gada Ziemassvetkos un Jaunaja gada!*                   |" << endl;
+   cout << "Sie ir tavi " << x << " Ziemassvētki un ari " << x << " Jaunais gads                        |" << endl;
+   cout << "Priecigus svetkus!                                                        |" << endl;
+   cout << "    *                                                                     |" << endl; 
+   cout << "   ***                                                                    |" << endl; 
+   cout << "  *****                                                                   |" << endl;
+   cout << "    *                                                                     |" << endl;
+   cout << "Ar milestibu, Alise Riekstina                                             |"                                              << endl;
+   cout << "__________________________________________________________________________|" << endl;
+   return 0;
+}
+

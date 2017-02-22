38f83a26aab84d5e5d2e38dd8edede9600aa8ac6
aljuxx <riekstinacool@gmail.com>
2017-02-22 14:02:44 +0200

    Created by Coding Ground Client

---
+++ New Project-20170222/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int sak=23;
+    int beig=30;
+  int x;
+  int y;
+  x=17;
+  cout << "Uzmini noslepto skaitli" << endl;
+ cin >> y;
+ 
+if (y>sak && y<beig){
+    cout << "Tu esi uzminejis slepeno skaitli" << endl;
+}else{ 
+    cout << "Megini velreiz" << endl;
+}
+    
+    
+    
+    
+   return 0;
+}
+

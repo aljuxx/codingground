56bf015e622d4d00272f7b341acf49eb6c4295e6
aljuxx <riekstinacool@gmail.com>
2017-04-26 12:38:00 +0300

    Created by Coding Ground Client

---
+++ New Project-20170426/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int x;
+ cout << "Ievadi skaitli" << endl;
+ cin >> x;
+   for(int i = 1 ;i <= x ; i = i + 1){
+       cout << i << endl;
+   }
+   return 0;
+}
+
